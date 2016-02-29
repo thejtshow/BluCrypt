85a147cfbc1637d2b4671b96d762fb7f1e190f31
Tingxin Yan <tingxiny@uark.edu>
2016-02-29 09:53:50 -0600

    branch init commit.

--- Android/app/src/main/java/csce5013/blucrypt/StartupActivity.java
+++ Android/app/src/main/java/csce5013/blucrypt/StartupActivity.java
@@ -2,6 +2,7 @@ package csce5013.blucrypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class StartupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+
+        Log.i("tingxiny", "Tingxiny branch.");
     }
 }
