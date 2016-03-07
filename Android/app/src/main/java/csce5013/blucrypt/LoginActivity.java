ed609a8bb6ea6abeb928bee5e2b6f62c4002bb00
Justin Davis <jtd006@uark.edu>
2016-03-07 15:07:33 -0600

    There is now a confirmation of new PIN dialogue (tested)

--- Android/app/src/main/java/csce5013/blucrypt/LoginActivity.java
+++ Android/app/src/main/java/csce5013/blucrypt/LoginActivity.java
@@ -1,6 +1,7 @@
 package csce5013.blucrypt;
 
-import android.provider.MediaStore;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,7 +9,6 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.io.File;
@@ -34,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         infoText2 = (TextView) findViewById(R.id.infoText2);
         clearMemory = (Button) findViewById(R.id.clearMemory);
 
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         PINField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -50,7 +51,7 @@ public class LoginActivity extends AppCompatActivity {
             {
                 if (s.length() == 6)
                 {
-                    int PIN = Integer.valueOf(s.toString());
+                    final int PIN = Integer.valueOf(s.toString());
 
                     //clear the text field here regardless -- aesthetics
                     s.clear();
@@ -72,18 +73,29 @@ public class LoginActivity extends AppCompatActivity {
                     }
                     else
                     {
-                        //no credentials exist yet, ask user if they want to add this credential
-                        if(true)
-                        {
-                            //add PIN to the credential store
-                            credentials.AddCredential(PIN);
+                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case DialogInterface.BUTTON_POSITIVE:
+                                        //add PIN to the credential store
+                                        credentials.AddCredential(PIN);
 
-                            //display a debug message
-                            infoText2.setText("Added PIN to credential store");
-                            
-                            //now log the user in
-                            Login();
-                        }
+                                        //display a debug message
+                                        infoText2.setText("Added PIN to credential store");
+
+                                        //now log the user in
+                                        Login();
+                                        break;
+
+                                    case DialogInterface.BUTTON_NEGATIVE:
+                                        break;
+                                }
+                            }
+                        };
+
+                        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                                .setNegativeButton("No", dialogClickListener).show();
                     }
                 }
             }
